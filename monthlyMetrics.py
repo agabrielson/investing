8a034d8bffda39e88f1aae8240e4d8eb10d759f3
Anthony Gabrielson <agabriel2@gmail.com>
2021-03-03 18:21:28 -0800

    initial scripts

---
+++ monthlyMetrics.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# quartelyMetrics.py
+#
+# Notes: Run on the first business day of the month
+#
+# Rev History:
+#   0.1 - 210303
+#           Initial Functionality
+
+import yfinance as yf
+import pandas as pd
+
+def getMetrics(symbol):
+    newtime_daily = yf.download(symbol, period = "1mo")
+    dayFirst = newtime_daily.iloc[0].Close
+    dayEnd = newtime_daily.iloc[-1].Close
+    
+    dataList = [symbol, dayFirst, dayEnd, dayEnd/dayFirst]    
+    return dataList
+
+
+# Allocate data & define symbols of interest
+dataList = []
+symbols = ['FSMEX', 'FXAIX', 'FNCMX', 'FCNTX', 'VTIVX', 'BFOCX', 'FBNDX', 'FSSNX', 'FBALX', 'FOCPX', 
+           'FBGRX', 'FSLEX', 'FDLSX', 'FSLBX', 'FTRNX', 'FPURX', 'FDGRX', 'FEMKX', 'FBNDX', 'FSRPX',
+           'FNBGX', 'FNORX', 'FBALX', 'FSMAX', 'FDSCX', 'AWTAX', 'FSDPX', 'FFGCX', 'FSPTX', 'FNILX',
+           'FZROX' ]
+
+# Sort symbols & remove duplicates
+symbols = sorted(symbols)
+res = [] 
+[res.append(symbol) for symbol in symbols if symbol not in res]
+symbols = res
+
+# lookup data
+for symbol in symbols:
+    dataList.append(getMetrics(symbol))
+
+#Seralize data
+df = pd.DataFrame (dataList,columns=['Symbol', 'Month Start', 'Month End', 'PERC'])
+df = df.T
+#print(df)
+df.to_excel('interest.xlsx')
---
+++ quarterlyMetrics.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# quartelyMetrics.py
+#
+# Rev History:
+#   0.1 - 210303
+#           Initial Functionality
+#           Yahoo ytdReturn appears mistaken at times
+#           Would like to add Fund Manager
+
+import yfinance as yf
+import pandas as pd
+import datetime as dt
+
+# What should be added?
+# Fund Manager
+
+def getDate():
+    dateTimeList = []
+    today = dt.date.today()
+    dateTimeList.append(today)
+    return dateTimeList[0]
+
+def getQuarterlyMetrics(symbol):
+    sym = yf.Ticker(symbol)
+    
+    dateToday = getDate()
+    sName = sym.info['symbol']
+    lName = sym.info['longName']
+    
+    sustain = "NotDefined"
+    if(sym.sustainability is not None):
+        sustain = sym.sustainability['Value'].to_dict()['peerGroup']
+        
+    mstar = sym.info['morningStarRiskRating']
+    expRatio = round(sym.info['annualReportExpenseRatio'],2)
+    ytdRtn = round(sym.info['ytdReturn'],2)
+    
+    beta3 = "NotDefined"
+    if(sym.info["beta3Year"] is not None):
+        beta3 = round(sym.info['beta3Year'],2)
+    
+    totAss = round(sym.info['totalAssets']/(1000000),0) 
+    turn = round(sym.info['annualHoldingsTurnover'],2)
+    
+    dataList = [dateToday, sName, lName, sustain, mstar, expRatio, ytdRtn, beta3, totAss, turn]
+   
+    return dataList
+
+# Allocate data & define symbols of interest
+dataList = []
+symbols = ['FSMEX', 'FXAIX', 'FNCMX', 'FCNTX', 'VTIVX', 'BFOCX', 'FBNDX', 'FSSNX', 'FBALX', 'FOCPX', 
+           'FBGRX', 'FSLEX', 'FDLSX', 'FSLBX', 'FTRNX', 'FPURX', 'FDGRX', 'FEMKX', 'FBNDX', 'FSRPX',
+           'FNBGX', 'FNORX', 'FBALX', 'FSMAX', 'FDSCX', 'AWTAX', 'FSDPX', 'FFGCX', 'FSPTX', 'FNILX',
+           'FZROX' ]
+
+# Sort symbols & remove duplicates
+symbols = sorted(symbols)
+res = [] 
+[res.append(symbol) for symbol in symbols if symbol not in res]
+symbols = res
+
+# lookup data
+for symbol in symbols:
+    print(symbol)
+    dataList.append(getQuarterlyMetrics(symbol))
+
+#Seralize data
+df = pd.DataFrame (dataList,columns=['Date', 'Symbol', 'Long Name', 'Peer Group', 'Morningstar',
+                                    'Exp Ratio', 'YTD Return(?!)', 'beta3', 'Total Assets ($M)',
+                                    'Turnover'])
+#print(df)
+df.to_excel('quarterly.xlsx')
