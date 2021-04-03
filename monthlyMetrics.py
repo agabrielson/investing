056ed9f5f1302c381b77685e09b811980d8867a7
Anthony Gabrielson <agabriel2@gmail.com>
2021-04-03 14:46:39 -0700

    Cleaned up code paths. Original main code path would have a bug leaving off the last day of the month.

--- monthlyMetrics.py
+++ monthlyMetrics.py
@@ -8,32 +8,31 @@
 # Rev History:
 #   0.1     210303      Initial Functionality
 #   0.2     210403      Added previous month lookup
+#   0.21    210403      Cleaned up code flow for one path
 
 import yfinance as yf
 import pandas as pd
 from datetime import date
-from calendar import monthrange
-
-def numDaysMonth(year, month):
-    return monthrange(year, month)[1]
 
 def getMetrics(symbol, month):
-        if month is None:
-            newtime_daily = yf.download(symbol, period = "1mo")
-        else:
-            todays_date = date.today()
-            yr = date.today().year
-            month_num = int(month)
-            start = "%4.4d-%2.2d-%2.2d" % (yr, month_num, 1)
-            end = "%4.4d-%2.2d-%2.2d" % (yr, month_num, numDaysMonth(yr, month_num))
-            newtime_daily = yf.download(symbol, start, end, interval="1m")
+    todays_date = date.today()
+    year = date.today().year
+    if month is None:
+        month = date.today().month - 1
         
-        #print(newtime_daily)
-        dayFirst = newtime_daily.iloc[0].Close
-        dayEnd = newtime_daily.iloc[-1].Close
+    if isinstance(month, str):
+        month = int(month)
+       
+    start = "%4.4d-%2.2d-%2.2d" % (year, month, 1)
+    end = "%4.4d-%2.2d-%2.2d" % (year, month+1, 1)
+    newtime_daily = yf.download(symbol, start, end, interval="1m")
 
-        dataList = [symbol, dayFirst, dayEnd, dayEnd/dayFirst]    
-        return dataList
+    #print(newtime_daily)
+    dayFirst = newtime_daily.iloc[0].Close
+    dayEnd = newtime_daily.iloc[-1].Close
+
+    dataList = [symbol, dayFirst, dayEnd, dayEnd/dayFirst]    
+    return dataList
 
 
 def monthlyMetric(month, filename):
@@ -43,6 +42,7 @@ def monthlyMetric(month, filename):
                 'FBGRX', 'FSLEX', 'FDLSX', 'FSLBX', 'FTRNX', 'FPURX', 'FDGRX', 'FEMKX', 'FBNDX', 'FSRPX',
                 'FNBGX', 'FNORX', 'FBALX', 'FSMAX', 'FDSCX', 'AWTAX', 'FSDPX', 'FFGCX', 'FSPTX', 'FNILX',
                 'FZROX', 'FSENX', 'FCPVX', 'FSHOX', 'FNARX']
+    #symbols = ['FSMEX']
 
     # Sort symbols & remove duplicates
     symbols = sorted(symbols)
@@ -61,14 +61,14 @@ def monthlyMetric(month, filename):
     df.to_excel(filename)
 
 if __name__ == "__main__":
-        import sys
+    import sys
         
-        month = None
-        if(len(sys.argv) >= 3):
-            month = sys.argv[2]
+    month = None
+    if(len(sys.argv) >= 3):
+        month = sys.argv[2]
 
-        filename = 'interestSymbols.xlsx'
-        if(len(sys.argv) >= 2):
-            filename = sys.argv[1]
+    filename = 'interestSymbols.xlsx'
+    if(len(sys.argv) >= 2):
+        filename = sys.argv[1]
 
-        monthlyMetric(month, filename)
+    monthlyMetric(month, filename)
