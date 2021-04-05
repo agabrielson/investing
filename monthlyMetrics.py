a949ea176a9c1cb502b38346ab7ec9ff69b8f600
Anthony Gabrielson <agabriel2@gmail.com>
2021-04-05 14:20:34 -0700

    added being able to set year

--- monthlyMetrics.py
+++ monthlyMetrics.py
@@ -13,26 +13,48 @@
 #       Notes: Run after the first business day of the month
 #   python3 monthlyMetrics.py march.xlsx 3
 #       Notes: Collect data for March of this year, outputs in march.xlsx 
+#   python3 monthlyMetrics.py dec2020.xlsx 12 2020
 #
 # Rev History:
 #   0.1     210303      Initial Functionality
 #   0.2     210403      Added previous month lookup
 #   0.21    210403      Cleaned up code flow for one path
+#   0.25    210405      Added year
 
 import yfinance as yf
 import pandas as pd
 from datetime import date
 
-def getMetrics(symbol, month):
+def startMonthYear(month, year):
+    if year is None:
+        year = date.today().year
+
+    if isinstance(year, str):
+        year = int(year)
+
     if month is None:
         month = date.today().month - 1
         
     if isinstance(month, str):
         month = int(month)
-       
-    year = date.today().year
+    
+    return month, year;
+
+def endMonthYear(month, year):
+    month, year = startMonthYear(month, year)
+    month = month + 1
+    if month == 13:
+        month = 1
+        year = year + 1
+
+    return month, year;
+
+def getMetrics(symbol, month, year):
+    month, year = startMonthYear(month, year)
+    monthEnd, yearEnd = endMonthYear(month, year)
+
     start = "%4.4d-%2.2d-%2.2d" % (year, month, 1)
-    end = "%4.4d-%2.2d-%2.2d" % (year, month+1, 1)
+    end = "%4.4d-%2.2d-%2.2d" % (yearEnd, monthEnd, 1)
     symDaily = yf.download(symbol, start, end, interval="1m")
 
     #print(symDaily)
@@ -55,7 +77,7 @@ def seralizeData(filename, dataList):
     #print(df)
     df.to_excel(filename)
 
-def monthlyMetric(month, filename):
+def monthlyMetric(filename, month, year):
     # Define symbols of interest
     symbols = ['FSMEX', 'FXAIX', 'FNCMX', 'FCNTX', 'VTIVX', 'BFOCX', 'FBNDX', 'FSSNX', 'FBALX', 'FOCPX', 
                 'FBGRX', 'FSLEX', 'FDLSX', 'FSLBX', 'FTRNX', 'FPURX', 'FDGRX', 'FEMKX', 'FBNDX', 'FSRPX',
@@ -67,13 +89,17 @@ def monthlyMetric(month, filename):
 
     dataList = []                       # Allocate variable name
     for symbol in symbols:              # Lookup symbols
-        dataList.append(getMetrics(symbol, month))
+        dataList.append(getMetrics(symbol, month, year))
     
     seralizeData(filename, dataList)    # Seralize data
 
 if __name__ == "__main__":
     import sys
-        
+    
+    year = None                        # month to lookup
+    if(len(sys.argv) >= 4):
+        year = sys.argv[3]
+
     month = None                        # month to lookup
     if(len(sys.argv) >= 3):
         month = sys.argv[2]
@@ -82,4 +108,4 @@ if __name__ == "__main__":
     if(len(sys.argv) >= 2):
         filename = sys.argv[1]
 
-    monthlyMetric(month, filename)      # run monthMetrics to collect data
+    monthlyMetric(filename, month, year)      # run monthMetrics to collect data
