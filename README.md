343b22a119412bdaa59d3e01ec87c2127e080127
Anthony Gabrielson <agabriel2@gmail.com>
2021-04-11 17:46:54 -0700

    A few small tweaks and adding holdings

--- README.md
+++ README.md
@@ -5,15 +5,17 @@
 
 Finding time to keep long term investment research up to date can be difficult. These scripts speed up collection of interesting data. I typically migrate these data points into a larger portfolio spreadsheet.
 
-## Build With
+## Run With
 
 	1. Tools run with Python3
 	1. yfinance: download initial data of interest
 	1. pandas: container to hold data before generating a spreadsheet
+	1. BeautifulSoup: Scrape a few web pages
 
 ## Usage
 	# monthlyMetrics.py: Collect monthly pricing data for funds of interest. Most pricing services provide limited granularity.
 	# quartelyMetrics.py: Pull longer term data points together that can be extremely time consuming.
+	# getHoldings.py: Extract fund holdings and attempt to get monthly return
 
 Examples are in each python file
 
--- getHoldings.py
+++ getHoldings.py
@@ -1,71 +1,163 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# monthlyMetrics.py
+# getHoldings.py
 #
 # Inputs
 #   filename:   spreadsheet to output data, can be left blank
 #   month:      month to collect data points for, can be left blank
-#   year:       year to collect data points for, can be left blank
+#	year:		year to collect data points for, can be left blank
 #
 # Examples
-#   python3 monthlyMetrics.py               -> outputs in interestSymbols.xlsx
-#   python3 monthlyMetrics.py data.xlsx     -> outputs in data.xlsx
+#   python3 getHoldings.py               -> outputs in interestSymbols.xlsx
+#   python3 getHoldings.py data.xlsx     -> outputs in data.xlsx
 #       Notes: Run after the first business day of the month
-#   python3 monthlyMetrics.py march.xlsx 3
+#   python3 getHoldings.py march.xlsx 3
 #       Notes: Collect data for March of this year, outputs in march.xlsx 
-#   python3 monthlyMetrics.py dec2020.xlsx 12 2020
+#   python3 getHoldings.py dec2020.xlsx 12 2020
 #
 # Rev History:
-#   0.1     210303      Initial Functionality
-#   0.2     210403      Added previous month lookup
-#   0.21    210403      Cleaned up code flow for one path
-#   0.25    210405      Added year
-#   0.26    210410      Adding significantly more symbols
+#   0.1     210411      Initial Functionality
 
 import yfinance as yf
 import pandas as pd
+import requests
+import re 
+import numpy
+from bs4 import BeautifulSoup
 from datetime import date
 
 def startMonthYear(month, year):
-    if year is None:
-        year = date.today().year
+	if year is None:
+		year = date.today().year
 
-    if isinstance(year, str):
-        year = int(year)
+	if isinstance(year, str):
+		year = int(year)
 
-    if month is None:
-        month = date.today().month - 1
-        
-    if isinstance(month, str):
-        month = int(month)
-    
-    return month, year;
+	if month is None:
+		month = date.today().month - 1
+
+	if isinstance(month, str):
+		month = int(month)
+
+	return month, year;
 
 def endMonthYear(month, year):
-    month, year = startMonthYear(month, year)
-    month = month + 1
-    if month == 13:
-        month = 1
-        year = year + 1
+	month, year = startMonthYear(month, year)
+	month = month + 1
+	if month == 13:
+		month = 1
+		year = year + 1
 
-    return month, year;
+	return month, year;
 
+# Try to figure out the monthly return for a symbol
 def getMetrics(symbol, month, year):
-    month, year = startMonthYear(month, year)
-    monthEnd, yearEnd = endMonthYear(month, year)
+	month, year = startMonthYear(month, year)
+	monthEnd, yearEnd = endMonthYear(month, year)
 
-    start = "%4.4d-%2.2d-%2.2d" % (year, month, 1)
-    end = "%4.4d-%2.2d-%2.2d" % (yearEnd, monthEnd, 1)
-    symDaily = yf.download(symbol, start, end, interval="1m")
+	start = "%4.4d-%2.2d-%2.2d" % (year, month, 1)
+	end = "%4.4d-%2.2d-%2.2d" % (yearEnd, monthEnd, 1)
+	try:
+		symDaily = yf.download(symbol, start, end, interval="1mo")
+		#print(symDaily)
+		dayFirst = symDaily.iloc[0].Close
+		dayEnd = symDaily.iloc[-1].Close
+		dataList = [symbol, dayFirst, dayEnd, dayEnd/dayFirst]
+	except:
+		dataList = [symbol, " ", " ", " "]
+	
+	return dataList
 
-    #print(symDaily)
-    dayFirst = symDaily.iloc[0].Close
-    dayEnd = symDaily.iloc[-1].Close
+# Process the holdings tuple
+#	(Company, Symbol, Total Net Assets)
+#	Note: Note all symbols are on the exchange...
+def getTuple(str):
+	companyName = ""
+	companySymbol = ""
+	fundPerc = ""
 
-    dataList = [symbol, dayFirst, dayEnd, dayEnd/dayFirst]    
-    return dataList
+	percLoc = re.search("\d+(\.\d\d%|\s\bpercent\b)",str).start()
+	fundPerc = str[percLoc:percLoc+5]
+	str = str[0:percLoc-1]
 
+	li = str.splitlines()
+
+	ctr = 0
+	for ln in li:
+		if (len(ln) > 0) and (ctr == 0):
+			companyName = ln
+			ctr = ctr + 1
+		else:
+			companySymbol = ln
+
+	companySymbol = companySymbol.replace(" ","")
+	companySymbol = companySymbol.replace(".", "-")
+	tupleOut = [companyName, companySymbol, fundPerc]
+
+	return tupleOut 
+
+# Put together the holdings list
+#	Make a call to get month return for a given period
+def buildHoldings(reducedStr, month, year):
+	dataList = []
+	deadFund = False
+
+	# Find As Of & last deliminiter (end of table)
+	matchAsOf = reducedStr.find("As of")	# Pull out As of 01/31/2021
+	asOf = reducedStr[matchAsOf:matchAsOf+16]
+	
+	# Find first deliminiter (start of table)
+	matchStart = reducedStr.find("Total Net Assets")+len("Total Net Assets")
+	
+	# This string has all holdings, need to get individual holdings
+	reducedStr = reducedStr[matchStart:matchAsOf-1]
+
+	iterCnt = True
+	while iterCnt == True:
+		#percLoc = reducedStr.find("%")
+		try:
+			percLoc = re.search("\d+(\.\d\d%|\s\bpercent\b)",reducedStr).start()+5
+		except:
+			iterCnt = False
+			break
+
+		if(type(percLoc) == None) or (percLoc == -1):
+			iterCnt = False
+			break
+
+		substr = reducedStr[0:percLoc+1]
+		tupleOut = getTuple(substr)
+		matchStart = percLoc+2
+		
+		if(len(tupleOut) > 2) and (tupleOut[1].find(".")) and (tupleOut[1].find(" ")):
+			dl = getMetrics(tupleOut[1], month, year)	# look up return for a symbol
+			ytdRtn = dl[3]
+			if ytdRtn != " ":
+				ytdRtnList = ("%.4f" % ytdRtn)
+				ytdRtnStr = " "
+				ytdRtnStr = ytdRtnStr.join(ytdRtnList).replace(" ","")
+				tupleOut += [ytdRtnStr]
+		
+		reducedStr = reducedStr[percLoc+1:matchAsOf-1]
+		dataList.append(tupleOut)
+
+	return dataList, asOf, deadFund
+
+# Make a request and start to reduce the string
+def procRequest(page):
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	text = soup.get_text();
+	x = text.partition('Top 10 Holdings')
+	y = x[2].partition('Distributions')
+	s = y[0]
+
+	str = re.sub(r'\n\s*\n', '\n', s, flags=re.MULTILINE)
+
+	return str
+
+# Deduplicate symbols and sort them
 def sortSymbols(symbols):
     symbols = sorted(symbols)
     res = [] 
@@ -73,15 +165,15 @@ def sortSymbols(symbols):
     symbols = res
     return symbols
 
+# Write data to the filesystem
 def seralizeData(filename, dataList):
-    df = pd.DataFrame (dataList,columns=['Symbol', 'Month Start', 'Month End', 'PERC'])
-    #df = df.T
-    #print(df)
-    df.to_excel(filename)
+	df = pd.DataFrame(dataList)
+	df.to_excel(filename)
 
-def monthlyMetric(filename, month, year):
-    # Define symbols of interest
-    symbols = ['FSAIX','FSCPX','FCYIX','FSDAX','FSDCX','FSELX','FSENX','FSESX','FSLEX','FIDSX','FDFAX',
+# Main
+def getHoldings(filename, month, year):
+	dataList = []
+	symbols = ['FSAIX','FSCPX','FCYIX','FSDAX','FSDCX','FSELX','FSENX','FSESX','FSLEX','FIDSX','FDFAX',
             'FDAGX','FDBGX','FDCGX','FDTGX','FDIGX','FIJCX','FSAVX','FSAGX','FGDIX','FGDAX','FGDBX',
             'FGDCX','FGDTX','FIJDX','FSPHX','FSVLX','FSCGX','FSDPX','FMFAX','FMFBX','FMFCX','FMFTX',
             'FMFEX','FIJFX','FSPCX','FDLSX','FSHCX','FSMEX','FSLXX','FSRBX','FBMPX','FGJMX','FGKMX',
@@ -134,32 +226,55 @@ def monthlyMetric(filename, month, year):
             'FIGSX','FFIGX','FINVX','FFVNX','FSTSX','FFSTX','FEDDX','FEDAX','FEDGX','FEDTX','FEDIX',
             'FIQGX','FTEJX','FTEMX','FTEDX','FTEFX','FTEHX','FIQNX','FGILX','FULTX','FKIDX','FAPCX',
             'FCNSX','FHKFX','FISZX','FDKFX','FSOSX','FNSTX','FEOPX','AWTAX','VTIVX','BFOCX']
-    #symbols = ['FSMEX']
-    
-    symbols = sortSymbols(symbols)      # Sort symbols & remove duplicates
+	#symbols = ["FIQJX", "FCSRX", "FSMEX", "FGKPX"]
 
-    dataList = []                       # Allocate variable name
-    for symbol in symbols:              # Lookup symbols
-        try:
-            dataList.append(getMetrics(symbol, month, year))
-        except:
-            dataList.append([symbol])
+	symbols = sortSymbols(symbols)      # Sort symbols & remove duplicates
 
-    seralizeData(filename, dataList)    # Seralize data
+	firstPass = True	# Set columns at top of spreadsheet
+
+	URL = 'https://www.marketwatch.com/investing/fund/'
+	for symbol in symbols:
+		print(symbol)
+		URLSym = URL + symbol
+		page = requests.get(URLSym)
+		data = " "
+
+		str = procRequest(page)
+		data, asOf, deadFund = buildHoldings(str, month, year)
+		
+		if(deadFund == False):
+			if(firstPass == True):
+				dataList.append(['Fund','Company Symbol','Total Net Assets','Total Net Assets','Monthly Rtn'])
+				firstPass = False
+
+			strList = [symbol] + [asOf]
+			dataList.append(strList)
+			for i in data:
+				try:
+					dataList.append([" "] + i)
+				except:
+					print("getHoldings: Error")
+					print("\t %s" % data)
+					print("\t %s" % i)
+					print("\t %s" % type(i))
+		else:
+			dataList.append([symbol])
+		
+	seralizeData(filename, dataList)
 
 if __name__ == "__main__":
     import sys
-    
+
     year = None                        # month to lookup
     if(len(sys.argv) >= 4):
-        year = sys.argv[3]
+    	year = sys.argv[3]
 
     month = None                        # month to lookup
     if(len(sys.argv) >= 3):
-        month = sys.argv[2]
+    	month = sys.argv[2]
 
-    filename = 'interestSymbols.xlsx'   # File name to seralize data
+    filename = 'fundHoldings.xlsx'   # File name to seralize data
     if(len(sys.argv) >= 2):
-        filename = sys.argv[1]
+    	filename = sys.argv[1]
 
-    monthlyMetric(filename, month, year)      # run monthMetrics to collect data
+    getHoldings(filename, month, year)
