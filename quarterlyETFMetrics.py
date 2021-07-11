a04508c1567b5374ca369665198ce466e27b7040
Anthony Gabrielson <agabriel2@gmail.com>
2021-07-11 16:34:42 -0700

    Additional fields

--- quarterlyETFMetrics.py
+++ quarterlyETFMetrics.py
@@ -38,44 +38,76 @@ def mwGetKeyData(fullPage):
     turnoverVal = mwProcData(fullPage[tableLoc:], 'Turnover %')
     div = mwProcData(fullPage[tableLoc:], 'Dividend')
     divDate = mwProcData(fullPage[tableLoc:], 'Ex-Dividend Date')
+    avgVol = mwProcData(fullPage[tableLoc:], 'Average Volume')
 
-    mwKeyDataList = [yrRangeVal, totAssetVal, yieldVal, expenseRatioVal, turnoverVal, div, divDate]
-    mwKeyDataHdr = ['52 Week Range', 'Total Net Assets', 'Yield', 'Net Expense Ratio', 'Turnover %', 'Dividend', 'Ex-Dividend Date']
+    mwKeyDataList = [yrRangeVal, totAssetVal, yieldVal, expenseRatioVal, turnoverVal, div, divDate, avgVol]
+    mwKeyDataHdr = ['52 Week Range', 'Total Net Assets', 'Yield', 'Net Expense Ratio', 'Turnover %', 'Dividend', 
+                    'Ex-Dividend Date', 'AVG Vol']
 
     return mwKeyDataList, mwKeyDataHdr
+
+def dbProcData(fullPage, searchStrS, searchStrE):
+    tableLocSrt = fullPage.find(searchStrS)+len(searchStrS)
+    tableLocEnd = fullPage.find(searchStrE)
+    redPage = fullPage[tableLocSrt:tableLocEnd]
+    reduced = redPage.splitlines()
+
+    if (len(reduced) == 1):
+        return None
+
+    return reduced[1]
+
+def dbGetKeyData1(fullPage):
+    ytdRtn = dbProcData(fullPage, '26 Week Return', 'Year to Date Return')
+    rtn1yr = dbProcData(fullPage, 'Year to Date Return', '1 Year Return')
+    rtn3yr = dbProcData(fullPage, '1 Year Return', '3 Year Return')
+    rtn5yr = dbProcData(fullPage, '3 Year Return', '5 Year Return')
+
+    dbKeyDataList = [ytdRtn, rtn1yr, rtn3yr, rtn5yr]
+    dbKeyDataHdr = ['YTD Rtn', '1 Yr Rtn', '3 Yr Rtn', '5 Yr Rtn']
+
+    return dbKeyDataList, dbKeyDataHdr
+
+def dbGetKeyData2(fullPage):
+    tableLoc = fullPage.find('Investment Themes')
+    cat = dbProcData(fullPage[tableLoc:], 'Category', 'Asset Class')
+    aClass = dbProcData(fullPage[tableLoc:], 'Asset Class', 'Asset Class Size')
+    classSz = dbProcData(fullPage[tableLoc:], 'Asset Class Size', 'Asset Class Style<')
+
+    dbKeyDataList = [cat, aClass, classSz]
+    dbKeyDataHdr = ['Category', 'Asset Class', 'Asset Class Size']
+
+    return dbKeyDataList, dbKeyDataHdr
 
 def yfGetQuarterlyMetrics(symbol):
     sym = yf.Ticker(symbol)
     
     sName = sym.info['symbol']
     lName = sym.info['longName']
-    
-    sustain = '-'
-    if(sym.sustainability is not None):
-        sustain = sym.sustainability['Value'].to_dict()['peerGroup']
 
     totAssets = sym.info['totalAssets']
     yieldVal = sym.info['yield']
     trailYield = sym.info['trailingAnnualDividendYield']
-    threeYrRtn = sym.info['threeYearAverageReturn']
-    fiveYrRtn = sym.info['fiveYearAverageReturn']
 
-    yfDataList = [sName, lName, sustain, totAssets, yieldVal, trailYield, threeYrRtn, fiveYrRtn]
-    yfDataHdr = ['Symbol', 'Name', 'Peer Group', 'Total Assets', 'yield', 'Trailing Annual Dividend Yield', '3 yr Rtn', '5 yr Rtn']
+    yfDataList = [sName, lName, totAssets, yieldVal, trailYield]
+    yfDataHdr = ['Symbol', 'Name', 'Total Assets', 'yield', 'Trailing Annual Dividend Yield']
 
     return yfDataList, yfDataHdr
 
 # Lookup quartely metrics
 #   No one source has everything
 def quarterlyMetric(filename):
-    symbols = readFunds('SymbolsETF.csv')      #Get symbols of interest
-    #symbols = readFunds('SymbolsETFDebug.csv')
+    #symbols = readFunds('SymbolsETF.csv')      #Get symbols of interest
+    symbols = readFunds('SymbolsETFDebug.csv')
     
     # Sort symbols & remove duplicates
     sortSymbols(symbols)
 
     # String to locate fund holdings
-    URL = 'https://www.marketwatch.com/investing/fund/'
+    MW_URL = 'https://www.marketwatch.com/investing/fund/'
+    DB_URL_p1 = 'https://etfdb.com/etf/'
+    DB_URL_p2 = '/#performance'
+    DB_URL_p3 = '/#etf-ticker-profile'
 
     dataList = []           # Allocate list to hold data
     hdrList = []            # Allocate list to hold header
@@ -85,19 +117,28 @@ def quarterlyMetric(filename):
     
     for symbol in symbols.index:  # lookup data
         print(symbol)
-        URLSym = URL + symbol
+        MW_URLSym = MW_URL + symbol
+        DB1_URLSym= DB_URL_p1 + symbol + DB_URL_p2
+        DB2_URLSym= DB_URL_p1 + symbol + DB_URL_p3
 
         try:
             yfData, yfDataHdr = yfGetQuarterlyMetrics(symbol)
-            mwPage = procRequest(URLSym)
+            
+            mwPage = procRequest(MW_URLSym)
             mwKeyDataList, mwKeyDataHdr = mwGetKeyData(mwPage)
 
-            symList = list(itertools.chain([dateToday], yfData, mwKeyDataList))
+            dbPage = procRequest(DB1_URLSym)
+            dbKeyDataList1, dbKeyDataHdr1 = dbGetKeyData1(dbPage)
+
+            dbPage = procRequest(DB2_URLSym)
+            dbKeyDataList2, dbKeyDataHdr2 = dbGetKeyData2(dbPage)
+
+            symList = list(itertools.chain([dateToday], yfData, mwKeyDataList, dbKeyDataList1, dbKeyDataList2))
             dataList.append(symList)
 
             if(count == True):
                 count = False
-                hdrList = list(itertools.chain(['Date'], yfDataHdr, mwKeyDataHdr))
+                hdrList = list(itertools.chain(['Date'], yfDataHdr, mwKeyDataHdr, dbKeyDataHdr1, dbKeyDataHdr2))
         except (IndexError, KeyError):
             dataList.append([dateToday, symbol])
 
